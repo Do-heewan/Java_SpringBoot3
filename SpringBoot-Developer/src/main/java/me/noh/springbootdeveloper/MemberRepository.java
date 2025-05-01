a1e6e622b4d061360d0cfe4b8705dc48224c4b10
noh <nhw3152@gmail.com>
2025-05-01 21:24:01 +0900

    ch03: 인터페이스 생성

---
+++ SpringBoot-Developer/src/main/java/me/noh/springbootdeveloper/MemberRepository.java
@@ -0,0 +1,8 @@
+package me.noh.springbootdeveloper;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
