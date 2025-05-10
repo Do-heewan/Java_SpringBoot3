3f3ec29d6e59cde693833d6d4d7955c54601b3f4
noh <nhw3152@gmail.com>
2025-05-10 23:09:14 +0900

    엔티티 생성

---
+++ SpringBoot-Developer/src/main/java/me/noh/springbootdeveloper/domain/Article.java
@@ -0,0 +1,34 @@
+package me.noh.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity // 엔티티 지정
+@NoArgsConstructor
+public class Article {
+    // 게터
+    @Id //  id 필드를 기본키로 설정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키를 자동으로 1씩 증가
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false) // 'title'이라는 not null 컬럼과 매핑
+    private String title;
+
+    @Column(name = "content", nullable = false) // 'content'라는 not null 컬럼과 매핑
+    private String content;
+
+    @Builder // 빌더 패턴으로 객체 생성
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
