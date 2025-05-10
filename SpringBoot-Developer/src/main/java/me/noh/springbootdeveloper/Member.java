c6c8fda8a1d32fcc8aaf47c60146424a91c9fac8
noh <nhw3152@gmail.com>
2025-05-10 20:11:19 +0900

    테스트 코드 작성

--- SpringBoot-Developer/src/main/java/me/noh/springbootdeveloper/Member.java
+++ SpringBoot-Developer/src/main/java/me/noh/springbootdeveloper/Member.java
@@ -14,6 +14,7 @@ public class Member {
     public void changeName(String name) {
         this.name = name;
     }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false)
