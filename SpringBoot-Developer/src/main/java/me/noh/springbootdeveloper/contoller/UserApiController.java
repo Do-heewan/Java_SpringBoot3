5c3c684e75fe81ec4793a442ca8dc1502325be61
noh <nhw3152@gmail.com>
2025-05-30 01:14:36 +0900

    카카오 로그인 추가

--- SpringBoot-Developer/src/main/java/me/noh/springbootdeveloper/contoller/UserApiController.java
+++ SpringBoot-Developer/src/main/java/me/noh/springbootdeveloper/contoller/UserApiController.java
@@ -4,17 +4,25 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import me.noh.springbootdeveloper.dto.AddUserRequest;
+import me.noh.springbootdeveloper.dto.KakaoUserInfoResponseDto;
+import me.noh.springbootdeveloper.service.KakaoService;
 import me.noh.springbootdeveloper.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @Controller
 public class UserApiController {
     private final UserService userService;
+    private final KakaoService kakaoService;
 
     @PostMapping("/user")
     public String signup(AddUserRequest request) {
@@ -27,5 +35,13 @@ public class UserApiController {
         new SecurityContextLogoutHandler().logout(request, response,
                 SecurityContextHolder.getContext().getAuthentication());
         return "redirect:/login";
+    }
+
+    @GetMapping("/auth/login/kakao")
+    public ResponseEntity<?> callback(@RequestParam("code") String code) throws IOException {
+        String accessToken = kakaoService.getAccessTokenFromKakao(code);
+        KakaoUserInfoResponseDto userInfo = kakaoService.getUserInfo(accessToken);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
